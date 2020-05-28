2799a77ed4d11bc90d0e07b5444007e03e0b254d
Reggie <cushingreggie@gmail.com>
2020-05-28 15:02:20 +0200

    updateed playbooks

--- Vagrantfile
+++ Vagrantfile
@@ -1,14 +1,14 @@
 IMAGE_NAME = "bento/ubuntu-18.04"
-N = 1
+N = 0
 
 Vagrant.configure("2") do |config|
     config.ssh.insert_key = false
 
     config.vm.provider "virtualbox" do |v|
-        v.memory = 1024
+        v.memory = 2048
         v.cpus = 2
     end
-      
+
     config.vm.define "k8s-master" do |master|
         master.vm.box = IMAGE_NAME
         master.vm.network "private_network", ip: "192.168.50.10"
@@ -19,19 +19,26 @@ Vagrant.configure("2") do |config|
                 node_ip: "192.168.50.10",
             }
         end
+        
+		master.vm.provision "ansible" do |ansible|
+            ansible.playbook = "k8s-setup/core-playbook.yml"
+        end
+
     end
 
-    (1..N).each do |i|
-        config.vm.define "node-#{i}" do |node|
-            node.vm.box = IMAGE_NAME
-            node.vm.network "private_network", ip: "192.168.50.#{i + 10}"
-            node.vm.hostname = "node-#{i}"
-            node.vm.provision "ansible" do |ansible|
-                ansible.playbook = "k8s-setup/node-playbook.yml"
-                ansible.extra_vars = {
-                    node_ip: "192.168.50.#{i + 10}",
-                }
-            end
-        end
-    end
+	(1..N).each do |i|
+		config.vm.define "node-#{i}" do |node|
+			node.vm.box = IMAGE_NAME
+			node.vm.network "private_network", ip: "192.168.50.#{i + 10}"
+			node.vm.hostname = "node-#{i}"
+			node.vm.provision "ansible" do |ansible|
+				ansible.playbook = "k8s-setup/node-playbook.yml"
+				ansible.extra_vars = {
+					node_ip: "192.168.50.#{i + 10}",
+				}
+			end
+
+		end
+	end
+
 end
---
+++ k8s-setup/genVault.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+if [ ! -f privateKey.txt ]; then
+    echo "generating keys"
+	openssl req -new -nodes -subj "/C=EU/ST=none/L=none/O=process/OU=none/CN=process-project.eu" -out cert.csr
+	openssl rsa -in privkey.pem -out privateKey.txt
+	openssl x509 -in cert.csr -out certificate.txt -req -signkey privateKey.txt -days 1001
+	openssl rsa -in privateKey.txt -pubout > publicKey.txt
+	rm cert.csr
+	rm *.pem
+fi
+
+echo "generating vault.yaml file!"
+DBPASS="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')"
+DBPASS64="$(echo $DBPASS | tr -d '\n' |  base64 | tr -d '\n')"
+DBROOTPASS="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')"
+DBROOTPASS64="$(echo $DBROOTPASS | base64 | tr -d '\n')"
+DBUSER="$(echo core-infra | base64 | tr -d '\n')"
+PRIV=$(cat privateKey.txt | base64 | tr -d '\n')
+PUB=$(cat publicKey.txt | base64 | tr -d '\n')
+CERT=$(cat certificate.txt | base64 | tr -d '\n')
+CONFIG=$(cat config.vagrant | base64 | tr -d '\n')
+COREINFRACONFIG="ewoJIm5hbWVzcGFjZVBvcnRzIjogewoJCSJ1YzEiOiB7CgkJCSJzY3AiOiAzMTEzOCwKCQkJIndlYmRhdiI6IDMxNDA4CgkJfSwKCQkidWMyIjogewoJCQkic2NwIjogMzIyMjIsCgkJCSJ3ZWJkYXYiOiAzMjIyMwoJCX0sCgkJInVjNCI6IHsKCQkJInNjcCI6IDMxMDEyLAoJCQkid2ViZGF2IjogMzE4MDYgCgkJfSwKCQkidWM1IjogewoJCQkic2NwIjogMzA5MjEsCgkJCSJ3ZWJkYXYiOiAzMDUzMCAKCQl9Cgl9Cn0K"
+
+cp vault.yaml.template vault.yaml.new
+sed -i "s/#DBPASS#/$DBPASS64/g" vault.yaml.new
+sed -i "s/#DBUSER#/$DBUSER/g" vault.yaml.new
+sed -i "s/#DB-ROOT-PASS#/$DBROOTPASS64/g" vault.yaml.new
+sed -i "s/#PRIVATEKEY.TXT#/$PRIV/g" vault.yaml.new
+sed -i "s/#PUBLICKEY.TXT#/$PUB/g" vault.yaml.new
+sed -i "s/#CERTIFICATE.TXT#/$CERT/g" vault.yaml.new
+sed -i "s/#CONFIG#/$CONFIG/g" vault.yaml.new
+sed -i "s/#CORE-INFRA-CONFIG.JSON#/$COREINFRACONFIG/g" vault.yaml.new
+
+mv vault.yaml.new vault.yaml
+
